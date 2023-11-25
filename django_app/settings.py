92dc79f9caaa281326c251dd2bfb7d700f7f1dfa
dedmasai <dedmasai@gmail.com>
2023-11-25 20:14:39 +0300

    work

--- django_app/settings.py
+++ django_app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xnsn3f55o&t^!0kjt3vm7ri7=9z78n4&$r*7wid*naj(%8rv%l'
+SECRET_KEY = 'django-insecure-xnsnrhtyjtghj765456i7=9z78n4&$r*7wid*naj(%8rv%l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
